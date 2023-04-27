f5bd542b4357859ad492be7d3479d1bdca404d5c
deancruz1997 <21037598@gmail.com>
2023-04-27 13:38:30 +0800

    Add game loop threading and FPS counter

--- Tales of Redemption/src/main/GamePanel.java
+++ Tales of Redemption/src/main/GamePanel.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
+
 import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
@@ -8,9 +11,13 @@ import inputs.MouseInputs;
 public class GamePanel extends JPanel{
 	
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100;
+	private float xDelta = 100, yDelta = 100;
+	private float xDir = 1f, yDir = 1f;
+	private Color color = new Color(150, 20, 90);
+	private Random random;
 	
 	public GamePanel() {
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
@@ -19,23 +26,48 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) {
 		this.xDelta += value;
-		repaint();
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
-		repaint();
 	}
 	
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.fillRect(xDelta, yDelta, 200, 50);
+		super.paintComponent(g); //a method that we don't actually call, but need, for painting
+		updateRectangle();
+		g.setColor(color);
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50); //initializes the first drawing
+		
 	}
+	
+	private void updateRectangle() {
+		xDelta += xDir;
+		if (xDelta > 400 || xDelta < 0) {
+			xDir *= -1;
+			color = getRndColor(); //updates color whenever square hits the border of the screen
+		}
+		
+		yDelta += yDir;
+		if (yDelta > 400 || yDelta < 0) {
+			yDir *= -1;
+			color = getRndColor();
+		}
+		
+	}
+	
+	// method to generate random colors
+	private Color getRndColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r, g, b);
+	}
+	
 	
 }
