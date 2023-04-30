a41f88f801c7c67704ed1d4e8dc53194d065fa97
Dean Cruz <cruzdean1997@gmail.com>
2023-04-30 11:53:12 +0800

    Add background image to menu and level offset

--- Tales of Redemption/src/entities/Player.java
+++ Tales of Redemption/src/entities/Player.java
@@ -39,8 +39,8 @@ public class Player extends Entity {
 		setAnimation();
 	}
 	
-	public void render(Graphics g) {
-		g.drawImage(animations[playerAction][aniIndex], (int)(hitbox.x - xDrawOffset), (int)(hitbox.y - yDrawOffset), width, height, null);
+	public void render(Graphics g, int lvlOffset) {
+		g.drawImage(animations[playerAction][aniIndex], (int)(hitbox.x - xDrawOffset) - lvlOffset, (int)(hitbox.y - yDrawOffset), width, height, null);
 //		drawHitbox(g);
 	}
 	
@@ -98,9 +98,13 @@ public class Player extends Entity {
 			jump();
 		}
 		
-		if (!left && !right && !inAir) {
-			return;
-		}
+//		if (!left && !right && !inAir) {
+//			return;
+//		}
+		
+		if (!inAir)
+			if((!left && !right) || (right && left))
+				return;
 		
 		float xSpeed = 0;
 		
