d6bfb62ed5f1677f605d8825cbbe85fdfe10e522
deancruz1997 <cruzdean1997@gmail.com>
2023-05-04 11:04:08 +0800

    Add game objects to level

--- Tales of Redemption/src/gamestates/Playing.java
+++ Tales of Redemption/src/gamestates/Playing.java
@@ -15,6 +15,7 @@ import entities.EnemyManager;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import objects.ObjectManager;
 import ui.GameOverOverlay;
 import ui.LevelCompletedOverlay;
 import ui.PauseOverlay;
@@ -25,6 +26,7 @@ public class Playing extends State implements Statemethods {
 	private Player player;
 	private LevelManager levelManager;
 	private EnemyManager enemyManager;
+	private ObjectManager objectManager;
 	private PauseOverlay pauseOverlay;
 	private GameOverOverlay gameOverOverlay;
 	private LevelCompletedOverlay levelCompletedOverlay;
@@ -77,6 +79,7 @@ public class Playing extends State implements Statemethods {
 	private void initClasses() {
 		levelManager = new LevelManager(game);
 		enemyManager = new EnemyManager(this);
+		objectManager = new ObjectManager(this);
 		player = new Player(80 * Game.SCALE, 280 * Game.SCALE, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE), this);
 		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
 		player.setSpawn(levelManager.getCurrentLevel().getPlayerSpawn());
@@ -93,6 +96,7 @@ public class Playing extends State implements Statemethods {
 			levelCompletedOverlay.update();
 		} else if (!gameOver) {
 			levelManager.update();
+			objectManager.update();
 			player.update();
 			enemyManager.update(levelManager.getCurrentLevel().getLevelData(), player);
 			checkCloseToBorder();
@@ -124,6 +128,7 @@ public class Playing extends State implements Statemethods {
 		levelManager.draw(g, xLvlOffset);
 		player.render(g, xLvlOffset);
 		enemyManager.draw(g, xLvlOffset);
+		objectManager.draw(g, xLvlOffset);
 		
 		if (paused) {
 			g.setColor(new Color(0, 0, 0, 150));
@@ -267,6 +272,10 @@ public class Playing extends State implements Statemethods {
 	
 	public void setLevelCompleted(boolean levelCompleted) {
 		this.lvlCompleted = levelCompleted;
+	}
+	
+	public ObjectManager getObjectManager() {
+		return objectManager;
 	}
 
 }
